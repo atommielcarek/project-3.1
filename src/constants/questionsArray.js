6a75e191fdcf3bcdf29e884ce05899af44454af7
Prometheus <atommielcarek@gmail.com>
2021-10-06 20:11:38 -0600

    initial commit

---
+++ src/constants/questionsArray.js
@@ -0,0 +1,44 @@
+let questionsArray = [
+    {
+      question: "Enter your full name.",
+      resumeField: "Name",
+      resumeFieldId: "name",
+    },
+    {
+      question: "Enter an e-mail address.",
+      resumeField: "Email",
+      resumeFieldId: "email",
+    },
+    {
+      question: "Enter your phone number.",
+      resumeField: "Phone Number",
+      resumeFieldId: "phoneNumber",
+    },
+    {
+      question: "Your home address.",
+      resumeField: "Address",
+      resumeFieldId: "address",
+    },
+    {
+      question: "Enter a career objective.",
+      resumeField: "Career Objective",
+      resumeFieldId: "objective",
+    },
+    {
+      question: "List your skills.",
+      resumeField: "Skills",
+      resumeFieldId: "skills",
+    },
+    {
+      question: "Prior work experience.",
+      resumeField: "Work Experience",
+      resumeFieldId: "experience",
+    },
+    {
+      question: "Educational history.",
+      resumeField: "Educational Qualifications",
+      resumeFieldId: "education",
+    },
+  ];
+  export default questionsArray;
+  
