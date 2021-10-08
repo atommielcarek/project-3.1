3191090dcd56bd58d436c05994a0c8523e86ce7f
LeMasters <jacksonlemasters@outlook.com>
2021-10-07 18:38:14 -0600

    Removed unused menu icon, improved title css

--- src/App.js
+++ src/App.js
@@ -67,16 +67,7 @@ function App() {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Typography variant="h4" component="div" textAlign="center"  sx={{ flexGrow: 1 }}>
             Resume Builder
           </Typography>
           <Button color="inherit">Login</Button>
