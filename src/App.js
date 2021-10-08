e4ac30d590466cc0ca0a56a114b80683056216a0
LeMasters <jacksonlemasters@outlook.com>
2021-10-07 18:29:35 -0600

    added material ui appbar and login icon

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React, { useEffect, useState } from "react";
 import AppContext from "./AppContext";
 import questionsArray from "./constants/questionsArray";
 import Questions from "./components/Questions";
-import { Typography } from "@material-ui/core";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
 function App() {
   let [questions, setQuestions] = useState([]);
@@ -56,16 +62,29 @@ function App() {
       }}
     >
       <div className="App">
-        <Typography
-          variant="h1"
-          style={{
-            textAlign: "left",
-            margin: "2rem",
-            fontFamily: "Helvetica Neue"
-          }}
-        >
-          Rezoom8
-        </Typography>
+    {
+  (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Resume Builder
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  )
+}
         <Questions />
       </div>
     </AppContext.Provider>
