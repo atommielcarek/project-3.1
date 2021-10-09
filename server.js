ba955972322d7f3cc2b6f609e730d8af582080b9
Leigh Johnson <leigh.carterjohnson@gmail.com>
2021-10-09 10:04:46 -0700

    create server

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const api1 = require('./routes/builderRoutes');
+const api2 = require('./routes/homeRoutes');
+
+const PORT = process.env.PORT || 3001;
+
+const { graphql, buildSchema } = require('graphql');
+const schema = buildSchema(`
+    type Query {
+    }`);
+graphql(schema).then((response) => {
+    console.log(reponse);
+});
+
+mongoose.set('debug', true);
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/ResumeBuilder',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
+);
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.use(api1);
+app.use(api2);
+
+app.listen(PORT, () => {
+    console.log(`Listening on http://localhost:${PORT}`);
+});
