1c0459f1a0fef65ef87f1bfb0a97f5b13e6373e1
Leigh Johnson <leigh.carterjohnson@gmail.com>
2021-10-11 23:06:52 -0700

    undo

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/ResumeBuilder', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Resumate', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
